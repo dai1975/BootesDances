40995f52b01c35224c45acf2a5b14de129c1fd64
KANDA Daisuke <dai1975@gmail.com>
2013-02-12 08:33:18 +0900

    implements to create new stage with movie against new save format

---
+++ BootesDances/include/bootes/dances/Stage.h
@@ -0,0 +1,26 @@
+#ifndef BOOTESDANCES_STAGE_H
+#define BOOTESDANCES_STAGE_H
+
+#include <TCHAR.h>
+#include <string>
+
+struct Stage
+{
+   int version;
+   std::string  name;
+   std::string  moviepath;
+   std::basic_string< TCHAR > tc_basename;
+   std::basic_string< TCHAR > tc_moviepath;
+};
+
+#endif
+
+/**
+ * Local Variables:
+ * mode: c++
+ * coding: utf-8-dos
+ * indent-tabs-mode: nil
+ * c-basic-offset: 3
+ * tab-width: 8
+ * End:
+ */
